441ae1fb7da87cf7d567e7e023078787f50d3ee0
Brad Babines <bbabines@gmail.com>
2023-06-26 21:10:44 -0400

    feat: part way through feature four layout

--- src/components/FeatureFour.tsx
+++ src/components/FeatureFour.tsx
@@ -2,40 +2,40 @@ import React from "react";
 
 const FeatureFour = () => {
 	return (
-		<div className="bg-[#0d084d] color-[white]">
-			{/* FEATURE DARK */}
-			<div className="container">
-				<div className="item">
-					<h1>
-						liverr <i>business</i>
-					</h1>
-					<h1>
+		<div className="bg-[#0d084d] text-[white] px-[50px] ">
+			<div className="flex  text-lg ">
+				<div className="w-[30%] flex flex-col  mx-auto my-auto">
+					<h3 className="text-[2.5rem] pb-[30px] pt-[0px] ">
+						workhive <i>business</i>
+					</h3>
+					<h4 className="text-[2rem] mb-[30px] ">
 						A business solution designed for <i>teams</i>
-					</h1>
-					<p>
+					</h4>
+					<p className="mb-[30px] text-[1.25rem] text-[lightgrey]">
 						Upgrade to a curated experience packed with tools and benefits,
 						dedicated to businesses
 					</p>
-					<div className="title">
-						<img src="./img/check.png" alt="" />
+					<div className="font-normal text-sm">
+						<img src="check.png" alt="a checkmark" />
 						Connect to freelancers with proven business experience
 					</div>
-
-					<div className="title">
-						<img src="./img/check.png" alt="" />
+					<div className="font-normal text-sm">
+						<img src="check.png" alt="a checkmark" />
 						Get matched with the perfect talent by a customer success manager
 					</div>
-
-					<div className="title">
-						<img src="./img/check.png" alt="" />
+					<div className="font-normal text-sm">
+						<img src="check.png" alt="a checkmark" />
 						Manage teamwork and boost productivity with one powerful workspace
 					</div>
-					<button>Explore Liverr Business</button>
+					<button className="bg-[#1dbf73] py-[10px] px-[20px] rounded-md cursor-pointer mb-[20px] mt-[20px]">
+						Explore Liverr Business
+					</button>
 				</div>
-				<div className="item">
+				<div className="font-normal text-sm">
 					<img
 						src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_870,dpr_2.0/v1/attachments/generic_asset/asset/d9c17ceebda44764b591a8074a898e63-1599597624768/business-desktop-870-x2.png"
-						alt=""
+						alt="a cascading of pictures"
+						className="py-[100px] w-[50%]"
 					/>
 				</div>
 			</div>
