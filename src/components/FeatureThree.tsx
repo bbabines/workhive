a5c9f8d8478fd52f4a97ab8aa4f3df7bf97e11ef
Brad Babines <bbabines@gmail.com>
2023-09-28 23:46:03 -0400

    feat: styled feature 3

--- src/components/FeatureThree.tsx
+++ src/components/FeatureThree.tsx
@@ -3,91 +3,94 @@ import React from "react";
 const FeatureThree = () => {
 	return (
 		<>
-			<div className="explore">
-				<div className="container">
-					<h1>Explore the marketplace</h1>
-					<div className="items">
-						<div className="item">
-							<img
-								src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/graphics-design.d32a2f8.svg"
-								alt=""
-							/>
-							<div className="line"></div>
-							<span>Graphics & Design</span>
-						</div>
-						<div className="item">
-							<img
-								src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/online-marketing.74e221b.svg"
-								alt=""
-							/>
-							<div className="line"></div>
+			<div className="mb-8">
+				<h1 className="my-[3rem] ml-[8em] text-[2.5rem] font-bold max-xl:text-center max-xl:ml-0">
+					You need it, we got it
+				</h1>
+				<div className="grid grid-cols-5 text-[1.5rem] mb-[4em] mx-[12em] text-center max-md:grid-cols-2 max-xl:mx-[1em]  grid-cols-auto-fill">
+					<div className="flex flex-col items-center">
+						<img
+							src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/graphics-design.d32a2f8.svg"
+							alt="an icon"
+							className="h-[50px]"
+						/>
+						<span>Graphics & Design</span>
+					</div>
+					<div className="flex flex-col items-center">
+						<img
+							src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/online-marketing.74e221b.svg"
+							alt="an icon"
+							className="h-[50px]"
+						/>
 
-							<span>Digital Marketing</span>
-						</div>
-						<div className="item">
-							<img
-								src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/writing-translation.32ebe2e.svg"
-								alt=""
-							/>
-							<div className="line"></div>
-							<span>Writing & Translation</span>
-						</div>
-						<div className="item">
-							<img
-								src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/video-animation.f0d9d71.svg"
-								alt=""
-							/>
-							<div className="line"></div>
-							<span>Video & Animation</span>
-						</div>
-						<div className="item">
-							<img
-								src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/music-audio.320af20.svg"
-								alt=""
-							/>
-							<div className="line"></div>
-							<span>Music & Audio</span>
-						</div>
-						<div className="item">
-							<img
-								src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/programming.9362366.svg"
-								alt=""
-							/>
-							<div className="line"></div>
-							<span>Programming & Tech</span>
-						</div>
-						<div className="item">
-							<img
-								src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/business.bbdf319.svg"
-								alt=""
-							/>
-							<div className="line"></div>
-							<span>Business</span>
-						</div>
-						<div className="item">
-							<img
-								src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/lifestyle.745b575.svg"
-								alt=""
-							/>
-							<div className="line"></div>
-							<span>Lifestyle</span>
-						</div>
-						<div className="item">
-							<img
-								src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/data.718910f.svg"
-								alt=""
-							/>
-							<div className="line"></div>
-							<span>Data</span>
-						</div>
-						<div className="item">
-							<img
-								src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/photography.01cf943.svg"
-								alt=""
-							/>
-							<div className="line"></div>
-							<span>Photography</span>
-						</div>
+						<span>Digital Marketing</span>
+					</div>
+					<div className="flex flex-col items-center">
+						<img
+							src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/writing-translation.32ebe2e.svg"
+							alt="an icon"
+							className="h-[50px]"
+						/>
+						<span>Writing & Translation</span>
+					</div>
+					<div className="flex flex-col items-center">
+						<img
+							src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/video-animation.f0d9d71.svg"
+							alt="an icon"
+							className="h-[50px]"
+						/>
+						<span>Video & Animation</span>
+					</div>
+					<div className="flex flex-col items-center">
+						<img
+							src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/music-audio.320af20.svg"
+							alt="an icon"
+							className="h-[50px]"
+						/>
+						<span>Music & Audio</span>
+					</div>
+				</div>
+
+				<div className="grid grid-cols-5 text-[1.5rem] mb-[5em] mx-[12em] text-center max-md:grid-cols-2 max-xl:mx-[1em] grid-cols-auto-fill">
+					<div className="flex flex-col items-center">
+						<img
+							src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/programming.9362366.svg"
+							alt="an icon"
+							className="h-[50px]"
+						/>
+						<span>Programming & Tech</span>
+					</div>
+					<div className="flex flex-col items-center">
+						<img
+							src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/business.bbdf319.svg"
+							alt="an icon"
+							className="h-[50px]"
+						/>
+						<span>Business</span>
+					</div>
+					<div className="flex flex-col items-center">
+						<img
+							src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/lifestyle.745b575.svg"
+							alt="an icon"
+							className="h-[50px]"
+						/>
+						<span>Lifestyle</span>
+					</div>
+					<div className="flex flex-col items-center">
+						<img
+							src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/data.718910f.svg"
+							alt="an icon"
+							className="h-[50px]"
+						/>
+						<span>Data</span>
+					</div>
+					<div className="flex flex-col items-center">
+						<img
+							src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/photography.01cf943.svg"
+							alt="an icon"
+							className="h-[50px]"
+						/>
+						<span>Photography</span>
 					</div>
 				</div>
 			</div>
